09f278524ab829aace39c12f48c6f75be03588aa
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-12-16 07:48:44 -0800

    styled-component

---
+++ components/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Login from './Login';
+import Register from './Reagister';
+import { Container } from './style';
+
+ function App() {
+  return (
+    <Container>
+    <Login/>
+    <Register/>
+
+
+    </Container>
+  )
+}
+
+export default App;
---
+++ components/src/Reagister.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ function App() {
+  return (
+    <div>
+Register
+
+
+    </div>
+  )
+}
+
+export default App;
